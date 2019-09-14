33561727f9f7ecca1a7676fac2153281f514b031
Leonardo Galdino <lcgm@cin.ufpe.br>
2019-09-14 15:18:57 -0300

    --amend

--- src/learn_classify.py
+++ src/learn_classify.py
@@ -2,7 +2,11 @@ import os
 
 from utils.corpus_loader import load_corpus
 
+# Por enquanto, apenas testa o corpus
 if __name__ == '__main__':
     cd = os.getcwd()
     d = os.path.join(cd, 'samples_pages')
     corpus = load_corpus(d)
+    for token in corpus.vocabulary:
+        print(token, corpus.vocabulary[token].freq, len(corpus.vocabulary[token].docs))
+    print(len(corpus.vocabulary))
