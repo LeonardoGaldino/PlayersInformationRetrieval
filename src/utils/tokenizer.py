47d012cbb81b4b8bfa9576f7a6ab49263a257244
Leonardo Galdino <lcgm@cin.ufpe.br>
2019-09-14 15:17:26 -0300

    Improve tokenizer

--- src/utils/tokenizer.py
+++ src/utils/tokenizer.py
@@ -1,8 +1,15 @@
+import re
+
+numeric_regex = r'[-+]?(\d)*.?(\d)*'
+reg = re.compile(numeric_regex)
+
 def tokenize(_input: str) -> [str]:
-    separators = ['.', ',', '!', '?', '"', '\n', '\t', '(', ')', '|', ';', '>', '<', '[', ']', '\'', ':', '@', '*']
+    separators = ['.', ',', '!', '?', '"', '\n', '\t', 
+                '(', ')', '|', ';', '>', '<', '[', ']', '\'', ':', 
+                '@', '*', '/', '=', '+']
 
     for separator in separators: 
         _input = _input.replace(separator, ' ')
     
-    words = [word for word in _input.split(' ') if len(word) > 0]
+    words = [word for word in _input.split(' ') if len(word) > 0 and not reg.fullmatch(word)]
     return words
