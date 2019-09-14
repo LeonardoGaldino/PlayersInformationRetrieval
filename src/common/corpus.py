cfc047be71c370e2d65c284a719a4042594bbe83
Leonardo Galdino <lcgm@cin.ufpe.br>
2019-09-14 15:23:16 -0300

    Comments corpus

--- src/common/corpus.py
+++ src/common/corpus.py
@@ -6,22 +6,31 @@ class Corpus:
         self.documents = documents
         self._build_corpus_vocabulary()
 
+    # Precomputa o vocabulário e suas estatísticas
     def _build_corpus_vocabulary(self):
         self.vocabulary = {}
         doc_index = 0
+
         for document in self.documents:
             for token in document.vocabulary:
+                # Frequência da palavra <token> no documento sendo analisado
                 freq = document.vocabulary[token]
+
+                # Checa se o vocabulário do corpus já contém a palavra
                 stats = self.vocabulary.get(token, None)
+
+                # Caso não, cria com a frequência do doc atual e adiciona seu índice
                 if stats is None:
                     self.vocabulary[token] = CorpusTokenStats(freq, [doc_index])
+                # Caso sim, adiciona a frequência do doc atual e seu índice
                 else:
                     self.vocabulary[token].add_stats(freq, doc_index)
 
             doc_index += 1
 
 
-
+# Classe responsável por representar as estatísticas de uma palavra dentro do corpus
+# Atualmente guarda a frequência total de ocorrências daquela palavra e os documentos nos quais apareceu
 class CorpusTokenStats:
 
     def __init__(self, freq: int = 0, docs: [int] = []):
