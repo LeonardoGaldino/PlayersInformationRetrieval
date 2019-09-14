5d08f2c680d70838ef20a4810d0fa16bbd623069
Leonardo Galdino <lcgm@cin.ufpe.br>
2019-09-14 15:03:36 -0300

    Implements corpus vocabulary

--- src/common/corpus.py
+++ src/common/corpus.py
@@ -1,5 +1,36 @@
 from common.document import Document
 
 class Corpus:
+
     def __init__(self, documents: [Document]):
         self.documents = documents
+        self._build_corpus_vocabulary()
+        for token in self.vocabulary:
+            print(token, self.vocabulary[token].freq, len(self.vocabulary[token].docs))
+        print(len(self.vocabulary))
+
+    def _build_corpus_vocabulary(self):
+        self.vocabulary = {}
+        doc_index = 0
+        for document in self.documents:
+            for token in document.vocabulary:
+                freq = document.vocabulary[token]
+                stats = self.vocabulary.get(token, None)
+                if stats is None:
+                    self.vocabulary[token] = CorpusTokenStats(freq, [doc_index])
+                else:
+                    self.vocabulary[token].add_stats(freq, doc_index)
+
+            doc_index += 1
+
+
+
+class CorpusTokenStats:
+
+    def __init__(self, freq: int = 0, docs: [int] = []):
+        self.freq = freq
+        self.docs = docs
+
+    def add_stats(self, freq: int, doc: int):
+        self.freq += freq
+        self.docs.append(doc)
