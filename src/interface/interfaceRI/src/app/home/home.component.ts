9a2e57b78999ade4f1f03e26a50099c484b747d6
bvcl <bvcl@cin.ufpe.br>
2019-11-21 00:56:33 -0300

    mutual information

--- src/interface/interfaceRI/src/app/home/home.component.ts
+++ src/interface/interfaceRI/src/app/home/home.component.ts
@@ -9,10 +9,61 @@ import { Utilities } from '../utils';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  focusOnField="";
+  namesTopMutualInfo=[];
+  positionTopMutualInfo=[];
+  nationalityTopMutualInfo=[];
+  numberTopMutualInfo=[];
+  teamTopMutualInfo=[];
+  genericTopMutualInfo=[];
   constructor(private router: Router, private dataService: DataService) { }
 
+
+  getTopMutualInfo(arr,attr){
+    var stopWords = ["de","para","fc","undefined"];
+    var attrMapped;
+    if(attr=="")attrMapped = arr.filter(o=>stopWords.indexOf(o.value)==-1);
+    else attrMapped = arr.filter(o=>o.attr==attr && stopWords.indexOf(o.value)==-1);
+    var attrMappedSorted = attrMapped.sort(function(a,b){if(a.freq>b.freq)return -1; else if(b.freq>a.freq)return 1;else return 0})
+    return attrMappedSorted.slice(0,3)
+  }
+
+  onFocus(field){
+    this.focusOnField = field;
+  }
+
+  readTextFile(file)
+  {
+      var that = this;
+      var rawFile = new XMLHttpRequest();
+      rawFile.open("GET", file, true);
+      rawFile.onreadystatechange = function ()
+      {
+          if(rawFile.readyState === 4)
+          {
+              if(rawFile.status === 200 || rawFile.status == 0)
+              {
+                  var allText = rawFile.responseText;
+                  var lines = allText.split('\n');
+                  var importantFromLines = [];
+                  lines.forEach(o=>{
+                    importantFromLines.push(o.split(' ')[0]+'//'+o.split(' ')[1]);
+                  })
+                  var mapped = importantFromLines.map(o=>{return {attr:o.split('//')[0],value:o.split('//')[1],freq:parseInt(o.split('//')[2])}})
+                  
+                  that.namesTopMutualInfo = that.getTopMutualInfo(mapped,"name"); 
+                  that.positionTopMutualInfo = that.getTopMutualInfo(mapped,"position");
+                  that.nationalityTopMutualInfo = that.getTopMutualInfo(mapped,"nationality");
+                  that.numberTopMutualInfo = that.getTopMutualInfo(mapped,"number");
+                  that.teamTopMutualInfo = that.getTopMutualInfo(mapped,"team");
+                  that.genericTopMutualInfo = that.getTopMutualInfo(mapped,"");
+              }
+          }
+      }
+      rawFile.send(null);
+  }
   ngOnInit() {
+    this.readTextFile("https://raw.githubusercontent.com/LeonardoGaldino/PlayersInformationRetrieval/master/src/index/freq_index.txt");
   }
 
   title = 'app';
